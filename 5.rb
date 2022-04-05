3ffca59d98c813cb44e0b1ec11baf8242f2182d4
Nima Karimi <nimakarimi676@gmail.com>
2022-04-05 23:38:22 +0200

    perf: simpler way for 5th

--- 5.rb
+++ 5.rb
@@ -1,21 +1,27 @@
 class LogLineParser
   def initialize(line)
-    @line = line
+    # @line = line
+    level, message = line.split(':')
+    @message = message.strip!
+    @level = level.gsub!(/\[|\]/, '').strip!.downcase
   end
 
   def message
-    indexOfColon = @line.index(':')
-    justMessage = @line.slice(indexOfColon + 1, @line.size).strip!
-    return justMessage
+    # indexOfColon = @line.index(':')
+    # justMessage = @line.slice(indexOfColon + 1, @line.size).strip!
+    # return justMessage
+    return @message
   end
 
   def log_level
-    logLevel = @line.slice(@line.index('[') + 1..@line.index(']') - 1).downcase!
-    return logLevel
+    # logLevel = @line.slice(@line.index('[') + 1..@line.index(']') - 1).downcase!
+    # return logLevel
+    return @level
   end
 
   def reformat
-    return "#{message} (#{log_level})"
+    # return "#{message} (#{log_level})"
+    return "#{@message} (#{@level})"
   end
 end
 
