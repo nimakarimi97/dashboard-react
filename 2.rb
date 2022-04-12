7f5a547abf80040994960f9a5f82417e2e5d42a6
Nima Karimi <nimakarimi676@gmail.com>
2022-04-12 11:42:48 +0200

    indentation improvement

--- 2.rb
+++ 2.rb
@@ -1,18 +1,18 @@
 class Lasagna
-    EXPECTED_MINUTES_IN_OVEN = 40
-    def remaining_minutes_in_oven(actual_minutes_in_oven)
-      return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
-    end
-  
-    def preparation_time_in_minutes(layers)
-      return layers*2
-    end
-  
-    def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-      return actual_minutes_in_oven + preparation_time_in_minutes(number_of_layers)
-    end
+  EXPECTED_MINUTES_IN_OVEN = 40
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
-  
-  lasagna = Lasagna.new
-  
 
+  def preparation_time_in_minutes(layers)
+    return layers * 2
+  end
+
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    return(
+      actual_minutes_in_oven + preparation_time_in_minutes(number_of_layers)
+    )
+  end
+end
+
+lasagna = Lasagna.new
