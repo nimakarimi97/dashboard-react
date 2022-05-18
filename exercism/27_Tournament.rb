534b0aa64f79c8748915ff374d2d8eb27b541f7e
nimakarimi76 <nimakarimi676@gmail.com>
2022-05-18 22:01:24 +0200

    refactor: added a class for simplifying the code

--- exercism/27_Tournament.rb
+++ exercism/27_Tournament.rb
@@ -1,20 +1,22 @@
-class Tournament
-  def self.tally(input)
-    headers = %w[Team MP W D L P]
-    str = format(headers)
-    table = {}
+class Team
+  attr_reader :table
+  def table_of_points(input)
+    @table = {}
     input
       .split("\n")
       .each do |match|
         firstOponent, secondOponent, result = match.split(';')
+
         table[firstOponent] = { mp: 0, w: 0, d: 0, l: 0, p: 0 } if table[
           firstOponent
         ].nil?
         table[secondOponent] = { mp: 0, w: 0, d: 0, l: 0, p: 0 } if table[
           secondOponent
         ].nil?
+
         table[firstOponent][:mp] += 1
         table[secondOponent][:mp] += 1
+
         if result == 'win'
           table[firstOponent][:w] += 1
           table[firstOponent][:p] += 3
@@ -31,17 +33,30 @@ class Tournament
         end
       end
     table
-      .sort_by { |name, numbers| [-numbers[:p], name] }
+  end
+end
+
+class Tournament
+  def self.tally(input)
+    headers = %w[Team MP W D L P]
+    str = format(headers)
+
+    table = Team.new.table_of_points(input)
+    p table
+    table
+      .sort_by { |name, numbers| p [-numbers[:p], name] }
       .each do |k, v|  #* - is for sorting descending
         row = [k, v[:mp], v[:w], v[:d], v[:l], v[:p]]
         str << format(row)
       end
-    str
+    p str
   end
+
   def self.format(array)
     sprintf "%-30s | %2s | %2s | %2s | %2s | %2s\n", *array
   end
 end
+
 input = <<~INPUT
   Devastating Donkeys;Courageous Californians;draw
   Allegoric Alaskans;Blithering Badgers;win
