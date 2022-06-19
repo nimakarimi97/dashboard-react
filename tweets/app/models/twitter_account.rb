09cbec1fd544b270b277146766cca592080d4435
nimakarimi76 <nimakarimi676@gmail.com>
2022-06-19 20:15:24 +0200

    feat: publish_to_twitter!

--- tweets/app/models/twitter_account.rb
+++ tweets/app/models/twitter_account.rb
@@ -3,4 +3,15 @@ class TwitterAccount < ApplicationRecord
 
   has_many :tweets, dependent: :destroy
   validates :username, uniqueness: true
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key =
+        Rails.application.credentials.dig(:twitter, :api_key)
+      config.consumer_secret =
+        Rails.application.credentials.dig(:twitter, :api_secret)
+      config.access_token = token
+      config.access_token_secret = secret
+    end
+  end
 end
