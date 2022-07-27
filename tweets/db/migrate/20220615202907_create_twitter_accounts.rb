3e0d58f076803f6d417955f282f4fdbe654b0e2f
nimakarimi76 <nimakarimi676@gmail.com>
2022-07-27 12:51:28 +0200

    Added unique key

--- tweets/db/migrate/20220615202907_create_twitter_accounts.rb
+++ tweets/db/migrate/20220615202907_create_twitter_accounts.rb
@@ -3,7 +3,7 @@ class CreateTwitterAccounts < ActiveRecord::Migration[7.0]
     create_table :twitter_accounts do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.string :name
-      t.string :username
+      t.string :username, unique: true
       t.string :image
       t.string :token
       t.string :secret
