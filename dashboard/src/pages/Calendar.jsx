81d0b2d3e9e87e5495256a044369d06d725aa504
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-27 10:27:50 +0200

    feat: calendar

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
-  )
-}
+    <div className="bg-white m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl">
+      <Header title="Calendar" category="App" />
+      <ScheduleComponent
+        height="650px"
+        selectedDate={new Date(2022, 7, 28)}
+        eventSettings={{ dataSource: scheduleData }}
+      >
+        <ViewsDirective>
+          {["Day", "Week", "WorkWeek", "Month", "Agenda"].map((item) => (
+            <ViewDirective key={item} option={item} />
+          ))}
+        </ViewsDirective>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
