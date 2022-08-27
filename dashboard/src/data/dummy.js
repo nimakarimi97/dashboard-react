a09fe2f821e59d65fdaaf036c62a0a7de41d08a3
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-27 10:58:55 +0200

    feat: editor app

--- dashboard/src/data/dummy.js
+++ dashboard/src/data/dummy.js
@@ -97,16 +97,18 @@ const gridEmployeeCountry = (props) => (
 );
 export const EditorData = () => (
   <div>
-    <h3>
-      Try React React has been designed from the start for gradual adoption, and
-      you can use as little or as much React as you need. Whether you want to
-      get a taste of React, add some interactivity to a simple HTML page, or
-      start a complex React-powered app, the links in this section will help you
-      get started. Online Playgrounds If you’re interested in playing around
-      with React, you can use an online code playground. Try a Hello World
-      template on CodePen, CodeSandbox, or Stackblitz. If you prefer to use your
-      own text editor, you can also download this HTML file, edit it, and open
-      it from the local filesystem in your browser. It does a slow runtime code
+    <h1>Heading 1</h1>
+    <h3>Try React</h3>
+    <p>
+      React has been designed from the start for gradual adoption, and you can
+      use as little or as much React as you need. Whether you want to get a
+      taste of React, add some interactivity to a simple HTML page, or start a
+      complex React-powered app, the links in this section will help you get
+      started. Online Playgrounds If you’re interested in playing around with
+      React, you can use an online code playground. Try a Hello World template
+      on CodePen, CodeSandbox, or Stackblitz. If you prefer to use your own text
+      editor, you can also download this HTML file, edit it, and open it from
+      the local filesystem in your browser. It does a slow runtime code
       transformation, so we’d only recommend using this for simple demos. Add
       React to a Website You can add React to an HTML page in one minute. You
       can then either gradually expand its presence, or keep it contained to a
@@ -138,7 +140,7 @@ export const EditorData = () => (
       recommend going through this JavaScript overview to check your knowledge
       level. It will take you between 30 minutes and an hour but you will feel
       more confident learning React.
-    </h3>
+    </p>
   </div>
 );
 const customerGridImage = (props) => (
