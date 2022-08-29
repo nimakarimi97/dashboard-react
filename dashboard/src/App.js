8321ebfe364e95804fa7c4f144f8ac192ac39370
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-29 14:56:57 +0200

    style: improving styles

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -53,11 +53,11 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <div className="w-72 fixed sidebar dark:bg-main-dark-bg bg-white">
               <Sidebar />
             </div>
           ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
+            <div className="w-0 dark:bg-main-dark-bg">
               <Sidebar />
             </div>
           )}
--- dashboard/src/data/dummy.js
+++ dashboard/src/data/dummy.js
@@ -847,27 +847,28 @@ export const medicalproBranding = {
 
 export const themeColors = [
   {
+    name: "indigo-theme",
+    color: "#1E4DB7",
+  },
+  {
     name: "blue-theme",
     color: "#1A97F5",
   },
-  {
-    name: "green-theme",
-    color: "#03C9D7",
-  },
+
   {
     name: "purple-theme",
     color: "#7352FF",
   },
   {
     name: "red-theme",
-    color: "#FF5C8E",
+    color: "#b11f1f",
   },
   {
-    name: "indigo-theme",
-    color: "#1E4DB7",
+    name: "green-theme",
+    color: "#008000",
   },
   {
-    color: "#FB9678",
+    color: "#cc7700",
     name: "orange-theme",
   },
 ];
