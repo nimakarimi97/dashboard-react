34b75146078c83fc34c973c3bf7c47bbe2d4e2e7
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-29 11:10:10 +0200

    changed the home path via Navigate

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
@@ -74,7 +74,7 @@ const App = () => {
 
               <Routes>
                 {/* dashboard  */}
-                <Route path="/" element={<Ecommerce />} />
+                <Route path="/" element={<Navigate to="/ecommerce" />} />
                 <Route path="/ecommerce" element={<Ecommerce />} />
 
                 {/* pages  */}
