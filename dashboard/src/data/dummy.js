4aabb6258980f400fc83eb777d3d93bdac534fdb
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-30 10:21:14 +0200

    feat: cart

--- dashboard/src/data/dummy.js
+++ dashboard/src/data/dummy.js
@@ -606,18 +606,21 @@ export const cartData = [
     name: "butterscotch ice-cream",
     category: "Milk product",
     price: "$250",
+    number: 1,
   },
   {
     image: product6,
     name: "Supreme fresh tomato",
     category: "Vegetable Item",
     price: "$450",
+    number: 1,
   },
   {
     image: product7,
     name: "Red color candy",
     category: "Food Item",
     price: "$190",
+    number: 1,
   },
 ];
 
