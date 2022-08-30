056085102fa1ba2c90ac1f8d497f39c0ee16081f
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-30 10:45:01 +0200

    feat: chat pop up

--- dashboard/src/components/Chat.jsx
+++ dashboard/src/components/Chat.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+
+import { Button } from ".";
+import { chatData } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const { currentColor } = useStateContext();
+
+  return (
+    <div className="absolute top-20 right-10 md:right-52 bg-white dark:bg-main-dark-bg drop-shadow-2xl shadow-inner shadow-gray-300 dark:shadow-gray-600 z-10 h-auto w-96 p-7 px-9 rounded-3xl">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-3">
+          <p className="font-semibold text-lg dark:text-gray-200">Messages</p>
+          <button
+            type="button"
+            className="text-white  text-xs rounded p-1 px-2 bg-orange"
+          >
+            5 New
+          </button>
+        </div>
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
+      </div>
+      <div className="mt-5 ">
+        {chatData?.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 border-b-1 border-color p-3 leading-8 cursor-pointer"
+          >
+            <div className="relative">
+              <img
+                className="rounded-full h-10 w-10"
+                src={item.image}
+                alt={item.message}
+              />
+              <span
+                style={{ background: item.dotColor }}
+                className="absolute inline-flex rounded-full h-2 w-2 right-0 -top-1"
+              />
+            </div>
+            <div>
+              <p className="font-semibold dark:text-gray-200 ">
+                {item.message}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400 text-sm">
+                {item.desc}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400 text-xs">
+                {item.time}
+              </p>
+            </div>
+          </div>
+        ))}
+        <div className="mt-5">
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text="See all messages"
+            borderRadius="10px"
+            width="full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
